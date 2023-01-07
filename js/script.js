eb6ff7668225e8ed662b14fed488a096931f7612
Wagner Miranda <wagnermarquesdemiranda@gmail.com>
2023-01-07 10:27:50 -0300

    validacao de formulario

--- js/script.js
+++ js/script.js
@@ -66,3 +66,82 @@ $('.nav-modal-open').on('click', function(e) {
     
     myModal.show()
 })
+
+
+
+/*
+* Validação de formulário
+*/
+function validate(elem) {
+    if(elem.val() == '' ) {
+        
+        console.log('O campo ' + elem.attr('name') + ' é obrigatório')
+        
+        elem.parent().find('.text-muted').show()
+        
+        elem.addClass('invalid')
+        
+        return false
+    } else {
+        elem.parent().find('.text-muted').hide()
+        elem.removeClass('invalid')
+        
+    }
+}
+
+
+$('body').on('submit', '.modal-body .form', function(e) {
+    
+    e.preventDefault()
+    
+    const inputName = $('#nome')
+    const inputEmail = $('#email')
+    
+    validate(inputName)
+    validate(inputEmail)
+    
+    if(inputName.hasClass('invalid') || inputEmail.hasClass('invalid')) {
+        console.log('Verificar os campos obrigatóios')
+        return false
+    } else {
+        $(this).submit()
+    }
+})
+
+
+$('body').on('blur', '#nome', function() {
+    validate($(this))   
+})
+
+$('body').on('blur', '#email', function() {
+    validate($(this))
+})
+
+$('body').on('focus', '#date', function() {
+    $(this).datepicker()
+})
+
+$('body').on('blur', '#date', function() {
+    validate($(this))
+    $(this).mask('00/00/0000');
+})
+
+$('body').on('blur', '#time', function() {
+    validate($(this))
+    $(this).mask('00:00');
+})
+
+$('body').on('blur', '#cep', function() {
+    validate($(this))
+    $(this).mask('00000-000');
+})
+
+$('body').on('blur', '#phone', function() {
+    validate($(this))
+    $(this).mask('00000-0000');
+})
+
+$('body').on('blur', '#cpf', function() {
+    validate($(this))
+    $(this).mask('000.000.000-00');
+})
