e7bae167d072d534c20e06a9230c0ca8b7e74c96
Wagner Miranda <wagnermarquesdemiranda@gmail.com>
2023-01-07 09:32:21 -0300

    alterado lista menu-hamburger/ criado modal/ ajustes em mobile

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 });
 */
 
-/* alterar o background-image com jQuery */
+/* alterar o background-image*/
 
 var url = "https://img.freepik.com/fotos-gratis/cachoeira-de-erawan-no-outono-tailandia-bela-cachoeira-com-piscina-esmeralda-na-natureza_335224-777.jpg?w=2000"
 
@@ -19,18 +19,50 @@ $(document).ready(function() {
 
 
 
-
 /*
- * Owl carousel products
- */
+* Owl carousel products
+*/
 
 $(document).ready(function() {
     
     $('.owl-carousel').owlCarousel();
-
+    
     // Configuração de produtos
     
     $('.featured-item a').addClass('btn btn-dark stretch-link');
     
     $('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>')
+})
+
+
+
+/*
+* Lista menu hamburger
+*/
+
+$('.navbar-toggler').click(function(){
+    $('#navbarResponsive').show();
+})
+
+$('.navbar-brand').click(function(){
+    $('#navbarResponsive').hide();
+})
+
+
+/*
+* Nav-modal-open
+*/
+
+$('.nav-modal-open').on('click', function(e) {
+    
+    e.preventDefault();
+    
+    let elem = $(this).attr('rel')
+    
+    $('.modal-body').html($('#'+elem).html())
+    $('.modal-header h5.modal-title').html($(this).text())
+    
+    let myModal = new bootstrap.Modal($('#modalId'))
+    
+    myModal.show()
 })
