e53aad346157a66b81debf73608a39cd35cfcc3b
Wagner Miranda <wagnermarquesdemiranda@gmail.com>
2023-01-07 11:52:32 -0300

    ajustes img/banner/ajustes script/mobile

--- js/script.js
+++ js/script.js
@@ -1,23 +1,3 @@
-/*
-/* Exibir linha span
-$(document).ready(function() {
-    $('.navbar-dark .navbar-nav .nav-link').mouseenter(function() {
-        $('#underline').show();
-    });
-});
-*/
-
-/* alterar o background-image*/
-
-var url = "https://img.freepik.com/fotos-gratis/cachoeira-de-erawan-no-outono-tailandia-bela-cachoeira-com-piscina-esmeralda-na-natureza_335224-777.jpg?w=2000"
-
-$(document).ready(function() {
-    $('.banner').on('click', function() {
-        $(this).css('background-image' , `url(${url})`)
-    })
-}) 
-
-
 
 /*
 * Owl carousel products
@@ -30,8 +10,6 @@ $(document).ready(function() {
     // Configuração de produtos
     
     $('.featured-item a').addClass('btn btn-dark stretch-link');
-    
-    $('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>')
 })
 
 
